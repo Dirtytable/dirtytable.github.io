e9255842030af13912a0c67f3ef60d53b4e182a1
Dirtytable <65018706+Dirtytable@users.noreply.github.com>
2020-05-13 22:26:23 +0600

    Ничего не получилось

---
+++ php/enter-check.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+    <title>Авторизация</title>
+    <meta charset="utf-8">
+</head>
+<body>
+<?php
+  $host = 'localhost';
+  $database = 'user-accounts';
+  $user = 'root';
+  $GLOBALS['link'] = mysqli_connect('localhost', 'root','', 'user-accounts');
+  $conn = mysqli_connect($host, $user, '', $database) or die("error " . mysqli_error($conn));
+  $res = mysqli_query($GLOBALS['link'], "SELECT `name` FROM `users` WHERE `name` = '$_POST[name]'");
+  $count = mysqli_num_rows($res);
+
+  if( $count > 0 ) {
+    $user = "SELECT * FROM `users` WHERE `name` = '$_POST[name]'";
+    $quer =   mysqli_query($GLOBALS['link'], $user );
+      while($row = $quer -> fetch_assoc()){
+        if($row['password'] == $_POST['password']){
+          echo "<script>alert('Вы авторизованы' )</script>";
+          echo "<script>document.location.href='http://localhost/denwer/web-tzi18-09r-koshkin-asset/site/php/main.php?name=$_POST[name]'</script>";
+        }
+        else{
+          echo "<script>alert('Пароль или Никнейм введён неправильно')</script>";
+          echo "<script>document.location.href='../enter.html'</script>";
+        }   
+      }
+  } else {
+      echo "<script>alert('Пароль или Никнейм введён неправильно')</script>";
+      echo  "<script>document.location.href='../enter.html'</script>";
+  }
+?>
+</body>
+</html>
